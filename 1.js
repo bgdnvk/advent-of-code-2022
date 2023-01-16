3531c68f6b454f9ab8a219ec116efa2bbac93d67
bgdnvk <bnovykov@gmail.com>
2023-01-16 08:04:33 +0100

    day 1 done

--- 1.js
+++ 1.js
@@ -4,10 +4,13 @@ fs.readFile('day1.txt', 'utf-8', (err, data) => {
     //console.log(data.toString())
     const lines = data.split('\n')
     let max = -Infinity
+
     let temp = 0
     //starts at 1
     let iteration = 0
     let res = {'elf': 0, 'val': -Infinity}
+//    const elves = []
+    const elvesCalories = []
 
     for(let line of lines) {
         temp +=Number(line)
@@ -19,10 +22,26 @@ fs.readFile('day1.txt', 'utf-8', (err, data) => {
                 res.elf = iteration
                 res.val = temp
             }
+//            elves.push({
+//                'elf': iteration,
+//                'val': temp
+//            })
+            //only the calories val are needeed
+            elvesCalories.push(temp)
+            //reset counter for next iteration
             temp = 0
         }
     }
     console.log(res)
+    console.log(elves)
+    console.log(elvesCalories.sort())
+    //js sort jfc
+    const sorted = elvesCalories.sort((a, b) => b-a)
+    console.log(sorted)
+    //get the top 3 from the sorted arr in desc order
+    top3calories = sorted[0] + sorted[1] + sorted[2]
+    console.log(top3calories)
+
 })
 
 function res() {
